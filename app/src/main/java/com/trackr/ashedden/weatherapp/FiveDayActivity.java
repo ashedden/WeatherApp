6f3693bba610beda5028de4e30297dd6b6c67230
Allison Shedden <ashedden@umail.ucsb.edu>
2016-09-10 18:19:03 -0700

    Finished UI for 5 day forecast

--- app/src/main/java/com/trackr/ashedden/weatherapp/FiveDayActivity.java
+++ app/src/main/java/com/trackr/ashedden/weatherapp/FiveDayActivity.java
@@ -1,11 +1,9 @@
 package com.trackr.ashedden.weatherapp;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -13,35 +11,22 @@ import android.widget.Button;
 /**
  * Created by Allison on 9/10/2016.
  */
-public class TodaysWeatherActivity extends AppCompatActivity {
+public class FiveDayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todaysweather);
-
-        Button fiveButton;
+        setContentView(R.layout.activity_fiveday);
 
         android.support.v7.app.ActionBar mActionBar = getSupportActionBar();
         mActionBar.setTitle("");
         mActionBar  .setDisplayHomeAsUpEnabled(true);
-
-        //Button is clicked, open TodaysWeatherActivity
-        fiveButton = (Button) findViewById(R.id.today_button);
-        fiveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), FiveDayActivity.class);
-                startActivity(i);
-            }
-        });
     }
 
     //From Android website
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
--- app/src/main/java/com/trackr/ashedden/weatherapp/MainActivity.java
+++ app/src/main/java/com/trackr/ashedden/weatherapp/MainActivity.java
@@ -18,12 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //No title preferably
-        android.support.v7.app.ActionBar mActionBar = getSupportActionBar();
-        mActionBar.setTitle("");
-
         //Button is clicked, open TodaysWeatherActivity
-        searchButton = (Button) findViewById(R.id.button);
+        searchButton = (Button) findViewById(R.id.main_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
