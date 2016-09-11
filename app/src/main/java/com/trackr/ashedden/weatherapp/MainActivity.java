89d6074e523f73a6e11d9532ad3e4ce14952ab11
Allison Shedden <ashedden@umail.ucsb.edu>
2016-09-10 17:10:03 -0700

    Added UI for Today's Weather

--- app/src/main/java/com/trackr/ashedden/weatherapp/MainActivity.java
+++ app/src/main/java/com/trackr/ashedden/weatherapp/MainActivity.java
@@ -1,17 +1,32 @@
 package com.trackr.ashedden.weatherapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Button is clicked, open TodaysWeatherActivity
+        searchButton = (Button) findViewById(R.id.button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), TodaysWeatherActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/trackr/ashedden/weatherapp/TodaysWeatherActivity.java
@@ -0,0 +1,16 @@
+package com.trackr.ashedden.weatherapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Allison on 9/10/2016.
+ */
+public class TodaysWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todaysweather);
+    }
+}
